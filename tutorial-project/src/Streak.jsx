98e7d3a05276a8c065860bf905ee8ede78908f3c
NerdyAlex <rich4alexisu@gmail.com>
2025-01-29 04:21:37 +0000

    init

--- tutorial-project/src/Streak.jsx
+++
@@ -1,8 +0,0 @@
-function Streak() {
-    return (
-        <div className="flex justify-center text-green-100 col ml-[5rem] p-2 text-[1.5rem] max-w-[50%] bg-green-500">
-            <p>Streak ⚡⚡</p>
-        </div>
-    )
-}
-export default Streak
