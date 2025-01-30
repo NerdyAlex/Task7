9c887566daa288cb83a4d3ea3c3ae9abe1ff03aa
NerdyAlex <rich4alexisu@gmail.com>
2025-01-30 01:26:30 +0000

    init

--- tutorial-project/src/Create.jsx
+++
@@ -1,8 +0,0 @@
-function Create(){
-    return(
-        <div className="flex justify-center text-green-100 col ml-[5rem] p-3 text-[1.5rem] max-w-[50%] bg-green-500">
-            <p>Create Something😊😊</p>
-        </div>
-    )
-}
-export default Create
--- tutorial-project/src/Fun.jsx
+++
@@ -1,8 +0,0 @@
-function Fun() {
-    return (
-        <div className="flex justify-center text-green-200 ml-[5rem] p-2 text-[1.5rem] max-w-[50%] bg-green-500">
-            Funn🎉🎉🎉
-        </div>
-    )
-}
-export default Fun
